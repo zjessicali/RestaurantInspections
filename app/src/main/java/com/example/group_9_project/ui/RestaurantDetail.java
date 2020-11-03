d48055aebe37f8ac9302309d3c36278c9543d057
Vishal <vvenkata@sfu.ca>
2020-11-03 04:32:26 -0800

    Completed and tested restaurant detail page

--- app/src/main/java/com/example/group_9_project/ui/RestaurantDetail.java
+++ app/src/main/java/com/example/group_9_project/ui/RestaurantDetail.java
@@ -6,10 +6,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.group_9_project.R;
 import com.example.group_9_project.model.InspectionManager;
@@ -32,7 +35,6 @@ public class RestaurantDetail extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_detail);
 
-        popultaeManager(); //DELETE THIS
         populateInspectionsList();
         extractData();
         setupRestaurantName();
@@ -46,66 +48,45 @@ public class RestaurantDetail extends AppCompatActivity {
         LinearLayout scrollLayout = findViewById(R.id.scrollLayout);
 
         for (int i = 0; i < inspections.getSize(); i++) {
-            InspectionReport inspection = inspections.getInspection(i);
-            String report = inspection.getNumCritical() + " critical issues     "
-                    + inspection.getNumNonCritical() + " non-critical issues     "
-                    + inspection.getInspectDateString();
-
+            final InspectionReport inspection = inspections.getInspection(i);
 
             TextView text = new TextView(RestaurantDetail.this);
-            text.setText(report);
+            String hazard = "";
 
             switch(inspection.getHazard()) {
                 case LOW:
-                    text.setTextColor(Color.GREEN);
+                    hazard = "<font color = '#00FF00'>LOW</font>";
                     break;
 
                 case MODERATE:
-                    text.setTextColor(Color.YELLOW);
+                    hazard = "<font color = '#FFFF00'>MEDIUM</font>";
                     break;
 
                 case HIGH:
-                    text.setTextColor(Color.RED);
+                    hazard = "<font color = '#FF0000'>HIGH</font>";
                     break;
             }
+
+            String report = inspection.getNumCritical() + " critical issues| "
+                    + inspection.getNumNonCritical() + " non-critical issues| "
+                    + inspection.getInspectDateString() + "| Hazard level:"
+                    + hazard;
+
+            text.setText(Html.fromHtml(report));
+            text.setTextSize(20);
+
+            text.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //Set intent when Single Inspection is created
+                }
+            });
 
             scrollLayout.addView(text);
         }
         
     }
 
-    private void popultaeManager() {
-        //DELETE THIS!!
-        Restaurant restaurant = new Restaurant("12345");
-        restaurant.setName("Blossom Teas");
-        restaurant.setAddress("7205 Barnet Road");
-        restaurant.setCity("Burnaby");
-        restaurant.setLatitude(12345.1234);
-        restaurant.setLongitude(2345.345);
-
-        InspectionManager inspections = new InspectionManager();
-        InspectionReport report1 = new InspectionReport();
-        report1.setHazard(InspectionReport.HazardRating.HIGH);
-        report1.setInspectDate(20200901);
-        report1.setInspType(InspectionReport.InspType.FOLLOWUP);
-        report1.setNumCritical(10);
-        report1.setNumNonCritical(2);
-
-        InspectionReport report2 = new InspectionReport();
-        report2.setHazard(InspectionReport.HazardRating.LOW);
-        report2.setInspectDate(20201028);
-        report2.setInspType(InspectionReport.InspType.ROUTINE);
-        report2.setNumCritical(0);
-        report2.setNumNonCritical(12);
-
-        inspections.addInspection(report1);
-        restaurant.addInspection(report1);
-
-        inspections.addInspection(report2);
-        restaurant.addInspection(report2);
-
-        manager.addRestaurant(restaurant);
-    }
 
     public static Intent launchIntent(Context context, int index) {
         Intent intent = new Intent(context, RestaurantDetail.class);
