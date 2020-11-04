bd47d546c83312857e85ba699178091d2e907604
Gurarjit Grewal <gsg24@sfu.ca>
2020-11-04 01:49:16 -0800

    Initial-sc 3-list-frag-edited intent

--- app/src/main/java/com/example/group_9_project/ui/Story3.java
+++ app/src/main/java/com/example/group_9_project/ui/Story3.java
@@ -22,9 +22,9 @@ import com.example.group_9_project.model.InspectionReport;
 import com.example.group_9_project.model.ViolationManager;
 
 public class Story3 extends AppCompatActivity {
-   private static InspectionReport report;
+    private static InspectionReport report;
 
-   ViolationManager manager=report.getManager();
+    ViolationManager manager = report.getManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,55 +34,56 @@ public class Story3 extends AppCompatActivity {
         setContentView(R.layout.activity_story3);
         setarrayadapter();
         populateHeader();
-regiterclick();
+        regiterclick();
     }
 
     private void populateHeader() {
-        TextView textView=(TextView)findViewById(R.id.InspectionName);
-        textView.setText(""+report.getFullDate());
-        TextView textView1=(TextView)findViewById(R.id.InspectionType);
-        textView1.setText(""+report.getInspType());
-        TextView textView2=(TextView)findViewById(R.id.Severity);
-        textView2.setText(""+report.getHazard());
+        TextView textView = (TextView) findViewById(R.id.InspectionName);
+        textView.setText("" + report.getFullDate());
+        TextView textView1 = (TextView) findViewById(R.id.InspectionType);
+        textView1.setText("" + report.getInspType());
+        TextView textView2 = (TextView) findViewById(R.id.Severity);
+        textView2.setText("" + report.getHazard());
 
     }
 
     private void regiterclick() {
-        ListView list=(ListView)findViewById(R.id.ArrayList);
+        ListView list = (ListView) findViewById(R.id.ArrayList);
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Violation clicked=manager.violationList.get(position);
+                Violation clicked = manager.violationList.get(position);
                 MessageFragment.getposition(position);
-                FragmentManager manager=getSupportFragmentManager();
-                MessageFragment dialog=new MessageFragment();
-                dialog.show(manager,"Message");
-                Log.i("Tag","Showed Dialog");
+                FragmentManager manager = getSupportFragmentManager();
+                MessageFragment dialog = new MessageFragment();
+                dialog.show(manager, "Message");
+                Log.i("Tag", "Showed Dialog");
             }
         });
     }
 
     private void setarrayadapter() {
-        ArrayAdapter<Violation>adapter=new MyListAdapter();
-        ListView list=(ListView)findViewById(R.id.ArrayList);
+        ArrayAdapter<Violation> adapter = new MyListAdapter();
+        ListView list = (ListView) findViewById(R.id.ArrayList);
         list.setAdapter(adapter);
     }
 
     private class MyListAdapter extends ArrayAdapter<Violation> {
-        public MyListAdapter(){
-            super(Story3.this,R.layout.list_items,manager.violationList);
+        public MyListAdapter() {
+            super(Story3.this, R.layout.list_items, manager.violationList);
 
 
         }
+
         @NonNull
         @Override
         public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-            View itemview=convertView;
-            if(itemview==null){
-                itemview=getLayoutInflater().inflate(R.layout.list_items,parent,false);
+            View itemview = convertView;
+            if (itemview == null) {
+                itemview = getLayoutInflater().inflate(R.layout.list_items, parent, false);
             }
-            Violation violation=manager.violationList.get(position);
-            TextView textView=(TextView)findViewById(R.id.list_violation_header);
+            Violation violation = manager.violationList.get(position);
+            TextView textView = (TextView) findViewById(R.id.list_violation_header);
             textView.setText(violation.toString());
             return itemview;
         }
@@ -90,10 +91,12 @@ regiterclick();
 
     }
 
-    public static Intent LaunchIntent(Context c, InspectionReport repo){
-        report=repo;
-        Intent intent=new Intent(c,Story3.class);
+    public static Intent LaunchIntent(Context c, InspectionReport repo) {
+        report = repo;
+        Intent intent = new Intent(c, Story3.class);
+        return intent;
     }
+
 
 
 }
