b6c7e4a5d9e63cca65e1128a2f7d7ef3c56ae760
jzl <jzl5@sfu.ca>
2020-11-14 19:03:21 -0800

    [sparshs][gsg24][vvenkata] Start getting data

--- app/src/main/java/com/example/group_9_project/ui/MainActivity.java
+++ app/src/main/java/com/example/group_9_project/ui/MainActivity.java
@@ -46,6 +46,8 @@ public class MainActivity extends AppCompatActivity {
     TextView title;
     ListView RestaurantList;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
